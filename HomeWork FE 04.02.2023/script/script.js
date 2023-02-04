f2d9f531a90cc873fda8a5c951065b5112a75ac4
Askolon <39673233+Askolon@users.noreply.github.com>
2023-02-04 23:25:35 +0100

    Add files via upload

---
+++ HomeWork FE 04.02.2023/script/script.js
@@ -0,0 +1,46 @@
+
+/*Задача 1*/
+/*const num = +prompt('Please, the number');
+
+if( num % 2 == 0){
+    console.log('четное');
+}else{
+    console.log('нечетное');
+}*/
+
+/*Задача 2*/
+
+/*const num1 = +prompt('Number 1');
+const num2 = +prompt('Number 2');
+
+if(num1 % num2 ==0){
+    console.log('делится');
+}else{
+    console.log('неделится');
+}*/
+
+/*Задача 3*/
+/*const a = +prompt('первая сторона');
+const b = +prompt('вторая сторона');
+const c = +prompt('третья сторона');
+
+console.log((a*b)/2);*/ /*Не понимаю зачем нам третья сторона, если в
+                            в формуле фигурирует только два числа*/
+
+/*Задача 4*/
+/*
+const a = +prompt('катет 1');
+const b = +prompt('катет 2');
+
+console.log(Math.sqrt(`${a**2+b**2}`));
+*/
+
+/*Задача 5*/
+/*
+const a = +prompt('Number');
+if( a % 3 ==0 || a % 7 ==0 || a % 10 !=0){
+    console.log('Yes');
+}else{
+    console.log('No');
+}
+*/
